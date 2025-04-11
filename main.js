8e81673ca925f26b56feadfe57450c97e8edee9c
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2025-04-11 13:10:28 -0300

    add: atualizações inserção banco de dados

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -19,11 +19,11 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: 'HOST_DO_BANCO',
-            user: 'USUARIO_DO_BANCO',
-            password: 'SENHA_DO_BANCO',
-            database: 'DATABASE_DO_BANCO',
-            port: 3306
+            host: '127.0.0.1', // nesse caso é para o servidor lubuntu no local usa localhost
+            user: 'aluno', // usuario do ubuntu ou local
+            password: 'Sptech#2024', // senha do lubuntu ou local
+            database: 'grupo10', // banco de dados
+            port: 3307 // 3307 quando é lubuntu, quando local 3306
         }
     ).promise();
 
@@ -63,7 +63,7 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)',
+                'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)', // remover o sensor_digital
                 [sensorAnalogico, sensorDigital]
             );
             console.log("valores inseridos no banco: ", sensorAnalogico + ", " + sensorDigital);
